ccc18e456d309c3e59868734c43d9888625edf81
FernandoSoftDeveloper <FernanPhysicode@proton.me>
2024-04-16 11:07:08 -0600

    "Create basic website"

---
+++ project/resources/views/core/home.blade.php
@@ -0,0 +1,20 @@
+@extends('./base/base')
+
+@section('title', 'Welcome | Home Page')
+
+@section('content')
+
+    <main>
+
+        <section>
+
+            <div class="container py-5">
+                <h1 class="text-center">Home Page</h1>
+            </div>
+
+        </section>
+
+    </main>
+    
+@endsection
+    
